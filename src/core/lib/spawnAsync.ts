346a7c1ba9bb4103e5557ce3894e04b0ebe917b1
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-08 20:18:19 -0300

    feat(library): añadir spawnAsync para manejar procesos de manera asíncrona
    es un "apaño" por ahora, hasta que pueda crear algo mejor con eventos propios (no EventEmitter)

---
+++ src/core/lib/spawnAsync.ts
@@ -0,0 +1,37 @@
+import { spawn } from 'node:child_process'
+import type { CommandKey } from '../types/processTypes'
+import { COMMANDS } from './constants'
+
+export function spawnAsync (command: CommandKey, args: string[], showOutput?: boolean) {
+  const _command = COMMANDS[command]
+
+  return new Promise((resolve, reject) => {
+    const process = spawn(_command, args)
+    let stdout = ''
+    let stderr = ''
+
+    process.stdout.on('data', (chunk) => {
+      stdout += chunk
+      const chunkStr = chunk.toString()
+      if (showOutput) console.log(chunkStr)
+    })
+
+    process.stderr.on('data', (chunk) => {
+      stderr += chunk
+      const chunkStr = chunk.toString()
+      if (showOutput) console.log(chunkStr)
+    })
+
+    process.on('close', (code) => {
+      if (code === 0) {
+        resolve(stdout.toString())
+      } else {
+        reject(new Error(stderr.toString()))
+      }
+    })
+
+    process.on('error', (err) => {
+      reject(err)
+    })
+  })
+}
