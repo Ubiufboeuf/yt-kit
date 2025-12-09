5c91cf505c9edab9dba88af8fbf8a30e90c72887
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-08 21:34:34 -0300

    feat(service): crear servicio para manejar la tarea de descargas con el contrato de la interfaz de descargas

---
+++ src/core/yt-dlp-downloader/YtDlpDownloader.ts
@@ -0,0 +1,47 @@
+import type { Downloader, DownloadOptions, DownloadResult } from '../interfaces/Downloader'
+import { RUTAS } from '../lib/constants'
+import { spawnAsync } from '../lib/spawnAsync'
+
+export class YtDlpDownloader implements Downloader {
+  async download (url: string, options: DownloadOptions): Promise<DownloadResult> {
+    const args = this.buildYtDlpArgs(url, options)
+
+    // const result = await spawnAsync('yt-dlp', args, true)
+    await spawnAsync('yt-dlp', args, true)
+
+    return {
+      duration: -1,
+      path: 'unknown'
+    }
+  }
+
+  private buildYtDlpArgs (url: string, options: DownloadOptions) {
+    const { id, type } = options
+    const isVideo = type === 'video'
+    
+    const ext = isVideo
+      ? 'mp4'
+      : 'aac'
+    
+    const exportRoute = isVideo
+      ? RUTAS.VIDEOS_DESCARGADOS
+      : RUTAS.AUDIOS_DESCARGADOS
+    
+    const exportName = '%(id)s.%(ext)s'
+
+    const audioFormat = 'aac'
+    
+    const args = [
+      '-f', `${id}/${ext}`,
+      ...(isVideo
+        ? []
+        : ['-x', '--audio-format', audioFormat]
+      ),
+      '-o', exportName,
+      '-P', exportRoute,
+      url
+    ]
+
+    return args
+  }
+}
