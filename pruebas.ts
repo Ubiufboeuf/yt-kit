cc7a547a37bd4f14519a3d5979b28dc19ae3dc31
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-08 22:48:38 -0300

    refactor: :truck: cambiar proyecto para enfocarlo en el core
    lo mismo, pero en vez de ir por un monorepositorio multipaquete, serán multipaquetes, y este es el core

--- pruebas.ts
+++ pruebas.ts
@@ -1,5 +1,5 @@
 // Acá voy a ir probando el core de primeras, para no tener que navegar mucho para probar el código
 
-import { downloadVideo } from './src/core/tasks/download/downloadVideo'
+import { downloadVideo } from './src/tasks/download/downloadVideo'
 
 await downloadVideo('wKVJi-FLvak', '135')
--- src/tasks/download/downloadVideo.ts
+++ src/tasks/download/downloadVideo.ts
@@ -1,7 +1,7 @@
 // import { spawnAsync } from 'src/core/lib/spawnAsync'
-import type { DownloadOptions } from 'src/core/interfaces/Downloader'
-import { formYoutubeUrl } from 'src/core/lib/ytUtils'
-import { YtDlpDownloader } from 'src/core/yt-dlp-downloader/YtDlpDownloader'
+import type { DownloadOptions } from 'src/interfaces/Downloader'
+import { formYoutubeUrl } from 'src/lib/ytUtils'
+import { YtDlpDownloader } from 'src/yt-dlp-downloader/YtDlpDownloader'
 
 export async function downloadVideo (ytId: string, id: string) {
   if (!ytId || !id) return
