f380beddd0f00458416d95d602a6225680e0e9a3
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-09 20:37:54 -0300

    test: probar la tarea para descargar audio

--- pruebas.ts
+++ pruebas.ts
@@ -1,8 +1,14 @@
 // Acá voy a ir probando el core de primeras, para no tener que navegar mucho para probar el código
 
-import { downloadVideo } from './src/tasks/download/download'
+import { downloadVideo, downloadAudio } from './src/tasks/download/download'
 
-await downloadVideo('wKVJi-FLvak', {
+const ytId = 'wKVJi-FLvak'
+
+await downloadVideo(ytId, {
   id: '135',
   filename: 'lol.mp4'
+})
+
+await downloadAudio(ytId, {
+  id: '251'
 })
