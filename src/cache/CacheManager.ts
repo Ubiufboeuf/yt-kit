33d092c40d80b9d37ab1eb2be137594aba26868b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-24 14:27:12 -0300

    feat: crear sistema de caché

---
+++ src/cache/CacheManager.ts
@@ -0,0 +1,33 @@
+import { timeToMs } from '../lib/timeUtils'
+
+interface CachedData {
+  timestamp: number
+}
+
+export class CacheManager {
+  private store = new Map<string, CachedData>()
+  private defaultTtl = timeToMs(10, 'day')
+  
+  get (key: string) {
+    const item = this.store.get(key)
+    if (!item) return null
+
+    if (Date.now() - item.timestamp > this.defaultTtl) {
+      this.delete(key)
+      return null
+    }
+
+    return item
+  }
+
+  set (key: string, value: CachedData) {
+    this.store.set(key, {
+      ...value,
+      timestamp: Date.now()
+    })
+  }
+
+  delete (key: string) {
+    this.store.delete(key)
+  }
+}
