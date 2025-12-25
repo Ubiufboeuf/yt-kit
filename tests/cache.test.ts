0da43f87b41630d0ca30818018ea84b67a1f8424
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-25 16:54:57 -0300

    test: añadir un test de la caché
    comprueba si se guarda correctamente y si se usa para el segundo formato

--- tests/cache.test.ts
+++ tests/cache.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'bun:test'
 import { cache } from '../src/cache/CacheManager'
+import { findFormatId } from '../src'
+import { timeToMs } from '../src/lib/timeUtils'
 
 describe('Cache Manager', () => {
   it('debería guardar y obtener un contenido cacheado', () => {
@@ -17,4 +19,28 @@ describe('Cache Manager', () => {
       timestamp: result.timestamp
     })
   })
+})
+
+describe('Process with cache', () => {
+  const ytId = 'wKVJi-FLvak'
+
+  it('debería cachear los formatos para que la segunda vez sea más rápido', async () => {
+    // Conseguir un video para guardar los formatos en caché
+    await findFormatId(ytId, 'best-video')
+
+    // Comprueba los datos cacheados
+    const cachedFormats = cache.get(`formats-${ytId}`)
+
+    expect(cachedFormats).toBeDefined()
+    expect(cachedFormats?.timestamp).toBeNumber()
+    expect(cachedFormats?.content).toBeString()
+
+    // Probar la caché con un audio
+    const start = Date.now()
+    await findFormatId(ytId, 'worst-audio')
+    const end = Date.now()
+
+    // Verificar si se usó la caché
+    expect(end - start).toBeLessThan(100)
+  }, timeToMs(15, 'sec'))
 })
