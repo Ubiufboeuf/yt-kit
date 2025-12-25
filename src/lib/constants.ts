b7dc0f2f5cfe3450c35a572e160fe42edbe6d62a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-25 19:07:28 -0300

    fix: :bug: cambiar comando yt-dlp-linux por yt-dlp
    debo poner eso como configuración, no como opción por defecto

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const STANDARD_RESOLUTIONS = [18, 144, 240, 360, 480, 720, 1080, 1440, 2160] as const
 
 export const COMMANDS = {
-  'yt-dlp': 'yt-dlp-linux'
+  'yt-dlp': 'yt-dlp'
 } as const
 
 export const DEFAULT_FILENAME = '%(ytId)s.%(ext)s'
