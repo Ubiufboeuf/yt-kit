a55a3d5e04c0060d85131414ec260fc827c00c51
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-27 18:21:52 -0300

    feat(cache): mejorar manejo de caché en disco
    Ahora espera la ruta general de caché, no la específica del item a cachear.
    Las específicas las maneja en base a la key que recibe.

--- src/cache/getFromDisk.ts
+++ src/cache/getFromDisk.ts
@@ -2,13 +2,24 @@ import { readFile } from 'node:fs/promises'
 import type { CachedData, CacheKey } from '../types/cacheTypes'
 import { config } from '../config/Configuration'
 import { resolvePath } from '../lib/resolvePath'
+import { join } from 'node:path'
 
 // Esta función es solo para caché
 export async function getFromDisk (key: CacheKey): Promise<CachedData | undefined> {
   const cacheLocation = config.get('cache')?.cacheLocation
   if (!cacheLocation) return
 
-  const cachePath = resolvePath(cacheLocation)
+  let cachePath = resolvePath(cacheLocation)
+  
+  // Si son formatos, cachea en una sub-carpeta
+  if (key.includes('formats:')) {
+    const ytId = key.split(':')[1]
+    if (!ytId) {
+      throw new Error(`Falta especificar ytId en ${key}`)
+    }
+    
+    cachePath = join(resolvePath(cacheLocation), 'formats', ytId)
+  }
 
   let content
   try {
--- src/cache/saveInDisk.ts
+++ src/cache/saveInDisk.ts
@@ -2,14 +2,25 @@ import { mkdir, writeFile } from 'node:fs/promises'
 import type { CachedData, CacheKey } from '../types/cacheTypes'
 import { config } from '../config/Configuration'
 import { resolvePath } from '../lib/resolvePath'
-import { dirname } from 'node:path'
+import { dirname, join } from 'node:path'
 
 // Esta función es solo para caché
 export async function saveInDisk (key: CacheKey, value: CachedData) {
   const cacheLocation = config.get('cache')?.cacheLocation
   if (!cacheLocation) return
 
-  const cachePath = resolvePath(cacheLocation)
+  let cachePath = resolvePath(cacheLocation)
+
+  // Si son formatos, cachea en una sub-carpeta
+  if (key.includes('formats:')) {
+    const ytId = key.split(':')[1]
+    if (!ytId) {
+      throw new Error(`Falta especificar ytId en ${key}`)
+    }
+    
+    cachePath = join(resolvePath(cacheLocation), 'formats', ytId)
+  }
+
   const cacheDir = dirname(cachePath)
 
   try {
