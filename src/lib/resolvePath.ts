f466a729414bcf21cd7110c8a80162fab4a2b8f3
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-27 04:30:30 -0300

    feat(lib): mejorar resolvePath()
    Pasa todos los tests que tiene

--- src/lib/resolvePath.ts
+++ src/lib/resolvePath.ts
@@ -1,6 +1,17 @@
 import { homedir } from 'node:os'
+import { join, resolve, sep } from 'node:path'
 
-export function resolvePath (path: string): string {
-  return path
-    .replace('~', homedir())
+export function resolvePath (input: string): string {
+  if (!input) return ''
+  if (input.match(/~[^/]+/)) {
+    throw new Error('Ruta inválida. Ten más cuidado usando \'~\'')
+  }
+
+  let p = input
+  
+  if (input.startsWith('~')) {
+    p = join(homedir(), sep, input.slice(1))
+  }
+
+  return resolve(p)
 }
