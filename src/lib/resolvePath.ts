c35a6ac3213e7923210c2f46b715d7eba421fc44
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-27 17:08:05 -0300

    feat(lib): mejorar resolvePath()
    Lanza errores si la ruta es inválida o no debería resolverse

--- src/lib/resolvePath.ts
+++ src/lib/resolvePath.ts
@@ -3,9 +3,14 @@ import { join, resolve, sep } from 'node:path'
 
 export function resolvePath (input: string): string {
   if (!input) return ''
-  if (input.match(/~[^/]+/)) {
+  if (input.match(/~[^/]+/))
     throw new Error('Ruta inválida. Ten más cuidado usando \'~\'')
-  }
+
+  if (input.includes(' '))
+    throw new Error('La ruta no puede tener espacios')
+
+  if (input === '.' || input === '..')
+    throw new Error(`'${input}' No es una ruta válida. Tal vez faltó agregar '/'?`)
 
   let p = input
   
