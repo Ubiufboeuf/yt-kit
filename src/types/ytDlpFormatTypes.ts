e89b13e7c30d5dae05df4a23af58091cc4d8b7b6
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-30 22:04:43 -0300

    refactor(types): refactorizar tipo YtDlpFormat
    Se movieron los tipos que usaba a la definición y se cambió a interfaz

--- src/types/ytDlpFormatTypes.ts
+++ src/types/ytDlpFormatTypes.ts
@@ -1,74 +1,48 @@
-export type YtDlpFormat = {
-    format_id:            string
-    format_note:          string
-    ext:                  EXT
-    protocol:             Protocol
-    acodec:               Acodec
-    vcodec:               string
-    url:                  string
-    width:                number | null
-    height:               number | null
-    fps:                  number | null
-    rows?:                number
-    columns?:             number
-    fragments?:           Fragment[]
-    audio_ext:            AudioEXT
-    video_ext:            VideoEXT
-    vbr:                  number | null
-    abr:                  number | null
-    tbr:                  number | null
-    resolution:           string
-    aspect_ratio:         number | null
-    filesize_approx:      number | null
-    http_headers:         HTTPHeaders
-    format:               string
-    asr?:                 number | null
-    filesize?:            number | null
-    source_preference?:   number
-    audio_channels?:      number | null
-    quality?:             number
-    has_drm?:             boolean
-    language?:            null | string
-    language_preference?: number
-    preference?:          null
-    dynamic_range?:       DynamicRange | null
-    container?:           Container
-    available_at?:        number
-    downloader_options?:  DownloaderOptions
-}
-
-export type Acodec = 'none' | 'mp4a.40.5' | 'opus' | 'mp4a.40.2'
-
-export type AudioEXT = 'none' | 'm4a' | 'webm'
-
-export type Container = 'm4a_dash' | 'webm_dash' | 'mp4_dash'
-
-export type DownloaderOptions = {
-    http_chunk_size: number
-}
-
-export type DynamicRange = 'SDR'
-
-export type EXT = 'mhtml' | 'm4a' | 'webm' | 'mp4'
-
-export type Fragment = {
-    url:      string
+export interface YtDlpFormat {
+  format_id: string
+  format_note: string
+  ext: 'mhtml' | 'm4a' | 'webm' | 'mp4'
+  protocol: 'mhtml' | 'https'
+  acodec: 'none' | 'mp4a.40.5' | 'opus' | 'mp4a.40.2'
+  vcodec: string
+  url: string
+  width: number | null
+  height: number | null
+  fps: number | null
+  rows?: number
+  columns?: number
+  fragments?: {
+    url: string
     duration: number
+  }[]
+  audio_ext: 'none' | 'm4a' | 'webm'
+  video_ext: 'none' | 'mp4' | 'webm'
+  vbr: number | null
+  abr: number | null
+  tbr: number | null
+  resolution: string
+  aspect_ratio: number | null
+  filesize_approx: number | null
+  http_headers: {
+    'User-Agent': string
+    Accept: 'text/html,application/xhtml+xml,application/xmlq=0.9,*/*q=0.8'
+    'Accept-Language': 'en-us,enq=0.5'
+    'Sec-Fetch-Mode': 'navigate'
+  }
+  format: string
+  asr?: number | null
+  filesize?: number | null
+  source_preference?: number
+  audio_channels?: number | null
+  quality?: number
+  has_drm?: boolean
+  language?: null | string
+  language_preference?: number
+  preference?: null
+  dynamic_range?: 'SDR' | null
+  container?: 'm4a_dash' | 'webm_dash' | 'mp4_dash'
+  available_at?: number
+  downloader_options?: {
+    http_chunk_size: number
+  }
 }
-
-export type HTTPHeaders = {
-    'User-Agent':      string
-    Accept:            Accept
-    'Accept-Language': AcceptLanguage
-    'Sec-Fetch-Mode':  SECFetchMode
-}
-
-export type Accept = 'text/html,application/xhtml+xml,application/xmlq=0.9,*/*q=0.8'
-
-export type AcceptLanguage = 'en-us,enq=0.5'
-
-export type SECFetchMode = 'navigate'
-
-export type Protocol = 'mhtml' | 'https'
-
-export type VideoEXT = 'none' | 'mp4' | 'webm'
