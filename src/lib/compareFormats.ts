4ef7dbb37a69ed76a1375fbbf77587e04ba72f40
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-07 03:25:24 -0300

    refactor(lib)!: renombrar getWorstFormat() a getWorseFormat()
    El nombre en inglés era incorrecto. Se cambió de worst a worse y las implementaciones, incluyendo la api pública

--- src/lib/compareFormats.ts
+++ src/lib/compareFormats.ts
@@ -59,7 +59,7 @@ export function getBetterFormat (a: YtDlpFormat | undefined, b: YtDlpFormat | un
   return aScore > bScore ? a : b
 }
 
-export function getWorstFormat (a: YtDlpFormat | undefined, b: YtDlpFormat | undefined, { type, compareResolution }: CompareOptions) {
+export function getWorseFormat (a: YtDlpFormat | undefined, b: YtDlpFormat | undefined, { type, compareResolution }: CompareOptions) {
   if (!a || !b) return
 
   const { aScore, bScore } = compareFormats({ a, b, type, compareResolution })
