429cace149669688ddef13e6bb9b0147eb4a3a31
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-02-13 18:54:08 -0300

    feat(task): agregar task mux para unir video con audios

---
+++ src/tasks/processing/multiplexing.ts
@@ -0,0 +1,27 @@
+import { FFmpegProcessor } from '../../ffmpeg-processor/FFmpegProcessor'
+import type { MultiplexingOptions } from '../../interfaces/Processor'
+import { basename, extname } from 'node:path'
+
+export async function mux (options: MultiplexingOptions) {
+  const { video, audios } = options
+  if (!video || audios.length === 0) return
+
+  const taskOptions = formProcessTaskOptions(options)
+  
+  return new FFmpegProcessor().mux(taskOptions)
+}
+
+function formProcessTaskOptions (options: MultiplexingOptions): MultiplexingOptions {
+  const { video, audios } = options
+  const videoSlug = basename(video, extname(video))
+
+  const taskOptions: MultiplexingOptions = {
+    video,
+    audios,
+    container: options.container ?? 'mp4',
+    slug: options?.slug ?? `av-${videoSlug}`,
+    outputDir: options?.outputDir ?? '.'
+  }
+
+  return taskOptions
+}
