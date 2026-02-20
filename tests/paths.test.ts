ed7bf6fbe651d2529ec7fb37f8aab1c62b777467
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-02-19 22:34:01 -0300

    test(lib): actualizar tests de resolvePath()

--- tests/paths.test.ts
+++ tests/paths.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'bun:test'
 import { resolvePath } from '../src'
+import { resolve } from 'node:path'
 
 describe('Rutas (Linux)', () => {
   it('Debe devolver \'\' si la ruta que recibe es falsy', () => {
@@ -7,8 +8,13 @@ describe('Rutas (Linux)', () => {
     expect(path).toBe('')
   })
   
-  it('\'~\' sin \'/\' al final, debería devolver $HOME y NO terminar con \'/\'', () => {
+  it('\'~\' sin \'/\' al final, debería devolver $HOME y no terminar con \'/\'', () => {
     const home = resolvePath('~')
+    expect(home).toMatch(/\/home\/.+[^/]/)
+  })
+  
+  it('\'~\' con \'/\' al final, debería devolver $HOME y no terminar con \'/\'', () => {
+    const home = resolvePath('~/')
     expect(home).toMatch(/\/home\/.+[^/]/)
   })
 
@@ -17,8 +23,8 @@ describe('Rutas (Linux)', () => {
   })
 
   it('.// debe tratarse como ./ y no dar error', () => {
-    const path = resolvePath('./..///../mango/././')
-    expect(path).toBe('/home/mango')
+    const path = resolvePath('./..///../yt-kit-environment/')
+    expect(path).toBe('/home/mango/Dev/yt-kit-environment')
   })
 
   it('Usar C: en Linux no debe fallar', () => {
@@ -29,12 +35,13 @@ describe('Rutas (Linux)', () => {
     expect(() => resolvePath('./formats/../ formats/./. /id-456')).toThrowError()
   })
 
-  it('Solo . debe fallar por no tener \'/\'', () => {
-    expect(() => resolvePath('.')).toThrowError()
+  it('"." debe terminar como la ruta actual', () => {
+    expect(() => resolvePath('.')).not.toThrowError()
   })
 
-  it('Solo .. debe fallar por no tener /', () => {
-    expect(() => resolvePath('..')).toThrowError()
+  it('Solo .. debe ir una ruta atrás', () => {
+    const path = resolvePath('..')
+    expect(path).toBe('/home/mango/Dev/yt-kit-ecosystem')
   })
 })
 
